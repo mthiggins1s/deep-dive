a85fed4efe4be0032fabd3965fc6283a346d7d6c
Matthew Higgins <mthiggins1s@semo.edu>
2025-05-02 11:48:38 -0500

    Refactor button component for content projection and update header and new ticket components to utilize it

--- 01-starting-project/src/app/header/header.component.ts
+++ 01-starting-project/src/app/header/header.component.ts
@@ -9,5 +9,5 @@ import { ButtonComponent } from "../shared/button/button.component";
   styleUrl: './header.component.css'
 })
 export class HeaderComponent {
- // Note: starting the supporting content projection video
+
 }
